27a0d4ae08d3891e70216058ac8eda7132772492
Grid112 <sumitware1999@gmail.com>
2021-11-20 19:38:53 +0530

    Welcome To JDBC Connectivity program

---
+++ src/main/java/com/bridgelabz/jdbc/DbConnection.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.jdbc;
+
+import java.sql.Connection;
+
+public class DbConnection {
+    public static void main(String[] args) {
+       System.out.println("Welcome To JDBC Connectivity program ");
+    }
+}
